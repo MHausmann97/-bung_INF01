e7ebb4c9ee869d99614b94df8c6a3215f8e2c640
MHausmann97 <73695590+MHausmann97@users.noreply.github.com>
2020-10-30 12:01:21 +0100

    Update INF01_Uebung.java

--- INF01_Uebung.java
+++ INF01_Uebung.java
@@ -15,7 +15,7 @@ public class INF01_Uebung {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // halllllo
     }
     
 }
